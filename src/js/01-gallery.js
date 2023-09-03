7ffb264ab08d58a1272e007b671bede6a9556711
Ridrox93 <popovandrii@yahoo.com>
2023-09-03 10:55:44 +0200

    2-add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,6 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
